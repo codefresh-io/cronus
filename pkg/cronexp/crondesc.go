782e68af9a1d95a2a8e58484b356161d86ba9416
andrii-codefresh <andrii@codefresh.io>
2020-06-04 16:24:31 +0300

    use cron

--- pkg/cronexp/crondesc.go
+++ pkg/cronexp/crondesc.go
@@ -1,7 +1,7 @@
 package cronexp
 
 import (
-	"github.com/gorhill/cronexpr"
+	"github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
@@ -22,30 +22,13 @@ func NewCronExpression() Service {
 
 func (expr *CronExpression) DescribeCronExpression(expression string) (string, error) {
 	log.WithField("expression", expression).Debug("describing cron expression")
-	expression = handleSpecialSyntax(expression)
 
-	parsed, err := cronexpr.Parse(expression)
+	c := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.DowOptional | cron.Descriptor)
+	s, err := c.Parse(expression)
 	if err != nil {
 		return "", err
 	}
 
-	nextTime := parsed.Next(time.Now()).String()
-	return nextTime, nil
-}
-
-func handleSpecialSyntax(expression string) string {
-	switch expression {
-	case "@yearly", "@annually":
-		return "0 0 0 1 1 * *"
-	case "@monthly":
-		return "0 0 0 1 * * *"
-	case "@weekly":
-		return "0 0 0 * * 0 *"
-	case "@daily", "@midnight":
-		return "0 0 0 * * * *"
-	case "@hourly":
-		return "0 0 * * * * *"
-	default:
-		return expression
-	}
+	st := s.Next(time.Now()).String()
+	return st, nil
 }
