c015f14bef5b255200571f7b17f56e810d933148
andrii-codefresh <andrii@codefresh.io>
2020-06-04 15:26:35 +0300

    cleanup

--- pkg/cronexp/crondesc.go
+++ pkg/cronexp/crondesc.go
@@ -1,10 +1,6 @@
 package cronexp
 
 import (
-	//"fmt"
-	//"net/http"
-	//"strings"
-
 	"github.com/gorhill/cronexpr"
 	log "github.com/sirupsen/logrus"
 	"time"
@@ -35,35 +31,6 @@ func (expr *CronExpression) DescribeCronExpression(expression string) (string, e
 
 	nextTime := parsed.Next(time.Now()).String()
 	return nextTime, nil
-	//// descriptor response
-	//type Result struct {
-	//	Description string `json:"description"`
-	//}
-	//// descriptor parameters
-	//type DescParams struct {
-	//	Expression string `url:"expression,omitempty"`
-	//	Locale     string `url:"locale,omitempty"`
-	//}
-	//params := &DescParams{Expression: expression, Locale: "en-US"}
-	//// invoke hermes trigger
-	//var res Result
-	//resp, err := api.endpoint.New().Get("api/descriptor").QueryStruct(params).ReceiveSuccess(&res)
-	//if err != nil {
-	//	log.WithError(err).Error("failed to invoke Cron Descriptor API")
-	//	return "", err
-	//}
-	//if resp.StatusCode != http.StatusOK {
-	//	log.WithField("http status", resp.Status).Error("Cron Descriptor API failed")
-	//	return "", fmt.Errorf("%s: error describing cron expression '%s'", resp.Status, expression)
-	//}
-	//// descriptor service returns 200 even for error and error details in string
-	//if strings.HasPrefix(res.Description, "Error:") {
-	//	log.Errorf("error describing cron expression '%s'", res.Description)
-	//	return "", fmt.Errorf("error describing cron expression '%s'", res.Description)
-	//}
-	//
-	//log.WithField("description", res.Description).Debug("successfully described cron expression")
-	//return res.Description, nil
 }
 
 func handleSpecialSyntax(expression string) string {
